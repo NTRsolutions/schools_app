24f40a1e8693488f62499f6099e83e87fff8f641
ordiusits <39825234+ordiusits@users.noreply.github.com>
2018-08-20 17:57:47 +0530

    users_module

---
+++ application/views/super/addUsers.php
@@ -0,0 +1,113 @@
+<div class="top-bar clearfix">
+    <div class="page-title">
+        <h4>Users</h4></div>
+</div>
+<div class="main-container">
+    <div class="container-fluid">
+        <div class="row gutter">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><p><a href="<?php echo base_url('super/users');?>" class="btn btn-danger btn-xs"><i class="fa fa-eye"></i> All Users List</a></p></div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                <div class="panel panel-yellow">
+                    <div class="panel-heading">
+                        <h4>Create New User</h4></div>
+                    <div class="panel-body">
+                        <form method="post" action="" class="form-horizontal">
+                            <fieldset>
+                              <div class="form-group col-lg-12">
+                                                      <label class="col-lg-3 control-label">Select School</label>
+                                                      <div class="col-lg-9">
+                                                          <select class="form-control" name="country">
+                                                              <option value="">-- Select School --</option>
+                                                              <option value="fr">Girls Primary</option>
+                                                              <option value="fr">Girls Secondry</option>
+                                                            </select>
+                                                      </div>
+                                </div>
+                                <div class="form-group col-lg-12">
+                                    <label class="col-lg-3 control-label">Name</label>
+                                    <div class="col-lg-9">
+                                        <input type="text" class="form-control" name="name" placeholder="Lavish Gangwar">
+                                    </div>
+                                </div>
+
+                                <div class="form-group col-lg-12">
+                                    <label class="col-lg-3 control-label">Email Id</label>
+                                    <div class="col-lg-9">
+                                        <input type="text" class="form-control" name="name" placeholder="abc@gmail.com">
+                                    </div>
+                                </div>
+
+                                <div class="form-group col-lg-12">
+                                    <label class="col-lg-3 control-label">Mobile No</label>
+                                    <div class="col-lg-9">
+                                        <input type="text" class="form-control" name="name" placeholder="+91 9718789479">
+                                    </div>
+                                </div>
+
+                                <div class="form-group col-lg-12">
+                                    <label class="col-lg-3 control-label">Address</label>
+                                    <div class="col-lg-9">
+                                        <textarea type="text" class="form-control" name="name" placeholder="Address"></textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group col-lg-12">
+                                                        <label class="col-lg-3 control-label">Qualification</label>
+                                                        <div class="col-lg-9">
+                                                            <select class="form-control" name="country">
+                                                                <option value="">-- Select School --</option>
+                                                                <option value="fr">Girls Primary</option>
+                                                                <option value="fr">Girls Secondry</option>
+                                                              </select>
+                                                        </div>
+                                  </div>
+
+                                  <div class="form-group col-lg-12">
+                                                          <label class="col-lg-3 control-label">Designation</label>
+                                                          <div class="col-lg-9">
+                                                              <select class="form-control" name="country">
+                                                                  <option value="">-- Select Designation --</option>
+                                                                  <option value="fr">Girls Primary</option>
+                                                            <option value="fr">Girls Secondry</option>
+                                      </select>
+                                      </div>
+                                    </div>
+                                    <div class="form-group col-lg-12">
+                                                            <label class="col-lg-3 control-label">Role</label>
+                                                            <div class="col-lg-9">
+                                                                <select class="form-control" name="country">
+                                                                    <option value="">-- Select Roll --</option>
+                                                                    <option value="fr">Girls Primary</option>
+                                                                    <option value="fr">Girls Secondry</option>
+                                                                  </select>
+                                                            </div>
+                                      </div>
+                                      <div class="form-group col-lg-12">
+                                          <label class="col-lg-3 control-label">Username</label>
+                                          <div class="col-lg-9">
+                                              <input type="text" class="form-control" name="name" placeholder="Ex: ADMIN0012">
+                                          </div>
+                                      </div>
+                                      <div class="form-group col-lg-12">
+                                          <label class="col-lg-3 control-label">Password</label>
+                                          <div class="col-lg-9">
+                                              <input type="text" class="form-control" name="name" placeholder="Password">
+                                          </div>
+                                      </div>
+                            </fieldset>
+
+
+                            <div class="form-group">
+                                <div class="col-lg-6 col-lg-offset-6">
+                                    <button type="submit" class="btn btn-success">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+
+                  </div>
+    </div>
+</div>
