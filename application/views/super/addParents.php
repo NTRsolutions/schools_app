9b17aab7a9bcddbc7ae31c32dbaa5b90caa8fb64
ordiusits <39825234+ordiusits@users.noreply.github.com>
2018-08-21 20:08:03 +0530

    CSS_Updated

---
+++ application/views/super/addParents.php
@@ -0,0 +1,103 @@
+<div class="top-bar clearfix">
+    <div class="page-title">
+        <h4>Parents and Gaurdians</h4></div>
+</div>
+<div class="main-container">
+    <div class="container-fluid">
+      <div class="row gutter">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><p><a href="<?php echo base_url('super/parents');?>" class="btn btn-danger btn-xs"><i class="fa fa-eye"></i> All Parents List</a></p></div>
+                  <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                      <div class="panel panel-yellow">
+                          <div class="panel-body">
+                              <form id="defaultForm" method="post" action="http://bootstrap.gallery/sunrise-admin-feb26/target.php" class="form-horizontal bv-form" novalidate="novalidate">
+                                  <fieldset>
+                                      <legend>Personal Information</legend>
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Gaurdians Name</label>
+                                          <div class="col-lg-8">
+                                              <input type="text" class="form-control" name="username" data-bv-field="username" placeholder="Gaurdians Name">
+                                          </div>
+                                      </div>
+
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Father Name</label>
+                                          <div class="col-lg-8">
+                                              <input type="text" class="form-control" name="username" data-bv-field="username" placeholder="Father's Name">
+                                          </div>
+                                      </div>
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Mother Name</label>
+                                          <div class="col-lg-8">
+                                              <input type="text" class="form-control" name="username" data-bv-field="username" placeholder="Mother's Name">
+                                          </div>
+                                      </div>
+
+
+                                  </fieldset>
+                                  <fieldset>
+                                      <legend>Contact Details</legend>
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Mobile Number</label>
+                                          <div class="col-lg-8">
+                                              <input type="text" class="form-control" name="username" data-bv-field="username" placeholder="+91 9718789479">
+                                          </div>
+                                      </div>
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Email Id</label>
+                                          <div class="col-lg-8">
+                                              <input type="text" class="form-control" name="username" data-bv-field="username" placeholder="abc@gmail.com">
+                                          </div>
+                                      </div>
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Address</label>
+                                          <div class="col-lg-8">
+                                              <textarea class="form-control" name="username" data-bv-field="username"></textarea>
+                                          </div>
+                                      </div>
+                                  </fieldset>
+                                  <fieldset>
+                                      <legend>Profession Information</legend>
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Father's Profession</label>
+                                          <div class="col-lg-8">
+                                              <input type="text" class="form-control" name="username" data-bv-field="username" placeholder="Business">
+                                          </div>
+                                      </div>
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Mother's Profession</label>
+                                          <div class="col-lg-8">
+                                              <input type="text" class="form-control" name="username" data-bv-field="username" placeholder="House Wife">
+                                          </div>
+                                      </div>
+
+                                  </fieldset>
+                                  <fieldset>
+                                      <legend>Credentials</legend>
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Username</label>
+                                          <div class="col-lg-8">
+                                              <input type="text" class="form-control" name="username" data-bv-field="username" placeholder="Username">
+                                          </div>
+                                      </div>
+                                      <div class="form-group has-feedback">
+                                          <label class="col-lg-4 control-label">Password</label>
+                                          <div class="col-lg-8">
+                                              <input type="password" class="form-control" name="username" data-bv-field="username" placeholder="********">
+                                          </div>
+                                      </div>
+                                  </fieldset>
+
+                                  <div class="form-group">
+                                      <div class="col-lg-6 col-lg-offset-6">
+                                          <button type="submit" class="btn btn-success">Submit</button>
+                                          <button type="reset" class="btn btn-danger">Cancel</button>
+                                      </div>
+                                  </div>
+                              </form>
+                          </div>
+                      </div>
+                  </div>
+
+              </div>
+    </div>
+</div>
--- application/views/super/parents.php
+++ application/views/super/parents.php
@@ -1,26 +1,25 @@
 <div class="top-bar clearfix">
     <div class="page-title">
-        <h4>Users</h4></div>
+        <h4>Parents List</h4></div>
 </div>
 <div class="main-container">
     <div class="container-fluid">
         <div class="row gutter">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><p><a href="<?php echo base_url('super/users/add');?>" class="btn btn-danger btn-xs"><i class="fa fa-plus"></i> Create New User</a></p></div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><p class="pull-right"><a href="<?php echo base_url('super/parents/add');?>" class="btn btn-danger btn-xs"><i class="fa fa-plus"></i> Add Parent</a></p></div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <div class="panel panel-blue">
                     <div class="panel-heading">
-                        <h4>Users and Roles</h4></div>
+                        <h4>Parents List</h4></div>
                     <div class="panel-body">
                         <div class="table-responsive">
                             <table id="fixedHeader" class="table table-striped table-bordered no-margin" cellspacing="0" width="100%">
                                 <thead>
                                     <tr>
                                         <th><input type="checkbox" value="None" id="check2" name="check"> All</th>
-                                        <th>Name</th>
-                                        <th>Email</th>
-                                        <th>Mobile</th>
-                                        <th>Role</th>
-                                        <th>Username</th>
+                                        <th>Gaurdian Name</th>
+                                        
+                                        <th>Email Id</th>
+                                        <th>Phone</th>
                                         <th>Status</th>
                                         <th>Action</th>
 
@@ -30,10 +29,10 @@
                                     <tr>
                                       <td><input type="checkbox" value="None" id="check2" name="check"></td>
                                       <td>Lavish Gangwar</td>
+
                                       <td>lavish@ordiusits.com</td>
                                       <td>+91 9718789479</td>
-                                      <td>HR Department</td>
-                                      <td>ADMIN001</td>
+
                                       <td>
                                         <label class="switch pull-right">
                                     <input type="checkbox" class="switch-input" checked="checked">
@@ -54,10 +53,10 @@
                                     <tr>
                                       <td><input type="checkbox" value="None" id="check2" name="check"></td>
                                       <td>Lavish Gangwar</td>
+
                                       <td>lavish@ordiusits.com</td>
                                       <td>+91 9718789479</td>
-                                      <td>HR Department</td>
-                                      <td>ADMIN001</td>
+
                                       <td><label class="switch pull-right">
                                     <input type="checkbox" class="switch-input" checked="checked"> <span class="switch-label" data-on="On" data-off="Off"></span> <span class="switch-handle"></span></label></td>
                                       <td>
@@ -71,10 +70,10 @@
                                     <tr>
                                       <td><input type="checkbox" value="None" id="check2" name="check"></td>
                                       <td>Lavish Gangwar</td>
+
                                       <td>lavish@ordiusits.com</td>
                                       <td>+91 9718789479</td>
-                                      <td>HR Department</td>
-                                      <td>ADMIN001</td>
+
                                       <td><label class="switch pull-right">
                                     <input type="checkbox" class="switch-input" checked="checked"> <span class="switch-label" data-on="On" data-off="Off"></span> <span class="switch-handle"></span></label></td>
                                       <td>
