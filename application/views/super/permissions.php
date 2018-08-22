22169f71f74ee0eaf191fad1159f5a842865a985
ordiusits <39825234+ordiusits@users.noreply.github.com>
2018-08-22 15:40:09 +0530

    Permissions_modeule_created_22Aug

---
+++ application/views/super/permissions.php
@@ -0,0 +1,107 @@
+<div class="top-bar clearfix">
+    <div class="page-title">
+        <h4>Permissions</h4></div>
+</div>
+<div class="main-container">
+    <div class="container-fluid">
+        <div class="row gutter">
+            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-lg-offset-4">
+              <div class="form-group col-lg-12">
+
+                                      <div class="col-lg-12">
+                                          <select class="form-control" name="country">
+                                              <option value="">-- Select Roll --</option>
+                                              <option value="">Teacher</option>
+                                              <option value="">Accountant</option>
+                                              <option value="">Accountant</option>
+                                            </select>
+                                      </div>
+                </div>
+            </div>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="panel panel-blue ">
+
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                          <table class="table table-bordered no-margin " cellspacing="0" width="100%">
+                              <thead>
+                                  <tr>
+                                    <th style="width:5%"># </th>
+                                    <th style="width:23%">Module Name</th>
+                                    <th style="width:18%">Add</th>
+                                    <th style="width:18%">Edit</th>
+                                    <th style="width:18%">Delete</th>
+                                    <th style="width:18%">View</th>
+                                  </tr>
+                              </thead>
+                            </table>
+
+                            <table class="table table-bordered no-margin" cellspacing="0" width="100%">
+                                <thead>
+                                  <tr><th colspan="6">Dashboard</th><tr>
+                                </thead>
+                                <tbody>
+                                  <tr>
+                                    <td style="width:5%"><input type="checkbox" value="None" id="check2" name="check"> </td>
+                                    <td style="width:23%">Dashboard</td>
+                                    <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                    <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                    <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                    <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                  </tr>
+
+                                </tbody>
+
+                                <table class="table table-bordered no-margin" cellspacing="0" width="100%">
+                                    <thead>
+                                      <tr><th colspan="6">Academics</th><tr>
+                                    </thead>
+                                    <tbody>
+                                      <tr>
+                                        <td style="width:5%"><input type="checkbox" value="None" id="check2" name="check"> </td>
+                                        <td style="width:23%">Dashboard</td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                      </tr>
+                                      <tr>
+                                        <td style="width:5%"><input type="checkbox" value="None" id="check2" name="check"> </td>
+                                        <td style="width:23%">Dashboard</td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                      </tr>
+                                      <tr>
+                                        <td style="width:5%"><input type="checkbox" value="None" id="check2" name="check"> </td>
+                                        <td style="width:23%">Dashboard</td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                      </tr>
+                                      <tr>
+                                        <td style="width:5%"><input type="checkbox" value="None" id="check2" name="check"> </td>
+                                        <td style="width:23%">Dashboard</td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                        <td style="width:18%"><input type="checkbox" value="None" id="check2" name="check"></td>
+                                      </tr>
+
+                                    </tbody>
+
+
+
+
+                            </table>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+                  </div>
+    </div>
+</div>
