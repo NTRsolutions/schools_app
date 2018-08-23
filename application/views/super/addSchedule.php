ec52b2f61bafc65d46b644fef66288e7bbd77a8b
ordiusits <39825234+ordiusits@users.noreply.github.com>
2018-08-23 19:47:47 +0530

    Time_table_module_23rdAug

---
+++ application/views/super/addSchedule.php
@@ -0,0 +1,120 @@
+<div class="top-bar clearfix">
+    <div class="page-title">
+        <h4>Add Schedule</h4></div>
+</div>
+<div class="main-container">
+    <div class="container-fluid">
+        <div class="row gutter">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><p><a href="<?php echo base_url('super/timetable');?>" class="btn btn-danger"><i class="fa fa-eye"></i> View Timetable</a></p></div>
+            <div class="col-lg-7 col-md-7 col-sm-12 col-xs-12">
+                <div class="panel panel-yellow">
+                    <div class="panel-heading">
+                        <h4>Create New Schedule</h4></div>
+                    <div class="panel-body">
+                        <form method="post" action="" class="form-horizontal">
+                            <fieldset>
+                              <div class="form-group col-lg-12">
+                                                      <label class="col-lg-4 control-label">School</label>
+                                                      <div class="col-lg-8">
+                                                          <select class="form-control" name="country">
+                                                              <option value="">-- Select School --</option>
+                                                              <option value="">DPS Delhi</option>
+                                                              <option value="">SMP Inter college, Bareilly</option>
+                                                            </select>
+                                                      </div>
+                                </div>
+                                <div class="form-group col-lg-12">
+                                                        <label class="col-lg-4 control-label">Session</label>
+                                                        <div class="col-lg-8">
+                                                            <select class="form-control" name="country">
+                                                                <option value="">-- Select Session --</option>
+                                                                <option value="">2018-19</option>
+                                                                <option value="">2019-20</option>
+                                                              </select>
+                                                        </div>
+                                  </div>
+                                <div class="form-group col-lg-12">
+                                                        <label class="col-lg-4 control-label">Class</label>
+                                                        <div class="col-lg-8">
+                                                            <select class="form-control" name="country">
+                                                                <option value="">-- Select Class --</option>
+                                                                <option value="">One</option>
+                                                                <option value="">Two</option>
+                                                              </select>
+                                  </div>
+                                  </div>
+                                  <div class="form-group col-lg-12">
+                                                          <label class="col-lg-4 control-label">Section</label>
+                                                          <div class="col-lg-8">
+                                                              <select class="form-control" name="country">
+                                                                  <option value="">-- Select Section --</option>
+                                                                  <option value="">One</option>
+                                                                  <option value="">Two</option>
+                                                                </select>
+                                    </div>
+                                    </div>
+                                    <div class="form-group col-lg-12">
+                                                            <label class="col-lg-4 control-label">Subject</label>
+                                                            <div class="col-lg-8">
+                                                                <select class="form-control" name="country">
+                                                                    <option value="">-- Select Subject --</option>
+                                                                    <option value="">One</option>
+                                                                    <option value="">Two</option>
+                                                                  </select>
+                                      </div>
+                                      </div>
+                                      <div class="form-group col-lg-12">
+                                                              <label class="col-lg-4 control-label">Day</label>
+                                                              <div class="col-lg-8">
+                                                                  <select class="form-control" name="country">
+                                                                      <option value="">-- Select Day --</option>
+                                                                      <option value="">Monday</option>
+                                                                      <option value="">Tuesday</option>
+                                                                      <option value="">Wednesday</option>
+                                                                      <option value="">Thursday</option>
+                                                                      <option value="">Friday</option>
+                                                                      <option value="">Saturday</option>
+                                                                    </select>
+                                        </div>
+                                        </div>
+                                    <div class="form-group col-lg-12">
+                                                            <label class="col-lg-4 control-label">Teacher</label>
+                                                            <div class="col-lg-8">
+                                                                <select class="form-control" name="country">
+                                                                    <option value="">-- Select Teacher --</option>
+                                                                    <option value="">Ramesh Gupta</option>
+                                                                    <option value="">Mukul Chhabra</option>
+                                                                  </select>
+                                      </div>
+                                      </div>
+                                      <div class="form-group col-lg-12">
+                                          <label class="col-lg-4 control-label">Time From</label>
+                                          <div class="col-lg-8">
+                                              <input type="time" class="form-control" name="name" placeholder="Example: Class One Syllabus">
+                                          </div>
+                                      </div>
+                                      <div class="form-group col-lg-12">
+                                          <label class="col-lg-4 control-label">Time to</label>
+                                          <div class="col-lg-8">
+                                              <input type="time" class="form-control" name="name" placeholder="Example: Class One Syllabus">
+                                          </div>
+                                      </div>
+
+
+                            </fieldset>
+
+                            <div class="form-group">
+                                <div class="col-lg-6 col-lg-offset-5">
+                                    <button type="submit" class="btn btn-success">Create</button>
+                                    <button type="reset" class="btn btn-danger">Clear</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+                  </div>
+    </div>
+</div>
